d8e9ba13ba3523a8e839b5162600d58829f2c849
xlboy360 <salgado.campos.juanisc@gmail.com>
2023-01-25 12:06:59 -0600

    added semantic html base

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <header>Little Lemon (R)</header>;
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <img src="../assets/logo.svg" alt="Little Lemon Logo" />
+      <ul>
+        <li>HOME</li>
+        <li>ABNOUT</li>
+        <li>MENU</li>
+        <li>RESERVATIONS</li>
+        <li>ORDER ONLINE</li>
+        <li>LOGIN</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
